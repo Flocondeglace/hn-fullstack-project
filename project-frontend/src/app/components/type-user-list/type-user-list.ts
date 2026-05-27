067d1be266fc58035e6e087ae6332ec4560b4d59
Flocondeglace <floriane.payen@gmail.com>
2026-05-27 16:40:31 +0200

    feat-front: user type sort

--- project-frontend/src/app/components/type-user-list/type-user-list.ts
+++ project-frontend/src/app/components/type-user-list/type-user-list.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { UserListTypeService } from '../../services/user-list-type-service';
 import { UserType } from '../../common/user-type';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+export type SortType = 'id' | 'typeName';
+export type SortDirection = 'asc' | 'desc';
 
 @Component({
   selector: 'app-type-user-list',
@@ -10,19 +13,32 @@ import { RouterLink } from '@angular/router';
   styleUrl: './type-user-list.scss',
 })
 export class TypeUserList implements OnInit {
-  typeUsers: WritableSignal<UserType[]> = signal<UserType[]>([]);
+  sortType: WritableSignal<SortType> = signal<SortType>('id');
+  sortDirection: WritableSignal<SortDirection> = signal<SortDirection>('asc');
+  userTypes: WritableSignal<UserType[]> = signal<UserType[]>([]);
 
-  constructor(private userListTypeService: UserListTypeService) {}
+  constructor(
+    private userListTypeService: UserListTypeService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
     this.updateList();
+
+    this.route.params.subscribe((params) => {
+      console.log('Route params: ', params);
+      this.sortDirection.set(params['sortDirection'] || 'asc');
+      this.sortType.set(params['sortType'] || 'id');
+      this.sortBy();
+    });
   }
 
   updateList(): void {
     this.userListTypeService.getTypeUserList().subscribe((data) => {
       console.log(data);
-      this.typeUsers.set(data);
+      this.userTypes.set(data);
     });
+    this.sortBy();
   }
 
   removeUserType(id: number): void {
@@ -30,5 +46,28 @@ export class TypeUserList implements OnInit {
       console.log(data);
       this.updateList();
     });
+  }
+
+  onSort(type: SortType) {
+    if (this.sortType() === type) {
+      this.sortDirection.set(this.sortDirection() === 'asc' ? 'desc' : 'asc');
+    } else {
+      this.sortType.set(type);
+      this.sortDirection.set('asc');
+    }
+    this.sortBy();
+  }
+
+  private sortBy() {
+    console.log('Sorting by: ', this.sortType(), 'Direction: ', this.sortDirection());
+    let sortedTypes = [...this.userTypes()];
+    const direction = this.sortDirection() === 'asc' ? 1 : -1;
+    if (this.sortType() === 'typeName') {
+      sortedTypes.sort((a, b) => a.typeName!.localeCompare(b.typeName!) * direction);
+    } else {
+      sortedTypes.sort((a, b) => (a.id! - b.id!) * direction);
+    }
+    this.userTypes.set(sortedTypes);
+    console.log('Sorted user types: ', this.userTypes());
   }
 }
